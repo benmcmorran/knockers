855d056921062d5d3a2cc3da8361598ddea19536
roboman2444 <roboman2444@gmail.com>
2017-01-14 00:59:42 -0500

    fuck

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,25 @@ var app        = express();                 // define our app using express
 var bodyParser = require('body-parser');
 
 
+var gcm = require('node-gcm');
+ 
+ 
+// Set up the sender with you API key, prepare your recipients' registration tokens. 
+var sender = new gcm.Sender('AAAAGdpIsWs:APA91bFtuYzDLTIHn7PFNCMsi8t3onoTG0p1nr03r2f9EvqybDnipMLhKMxPPLX9SM78_f1JhAe_KAyTkbwpMl8uOXBiQIRBpqhdz_2frBC6oamOLysc8wzQB2Bhjmv0giDf8Hj-lheZ');
+
+
+
+var regTokens = ['APA91bG73BGgBOpCSQMtV8O4tyxoWyJaQ1FCfukYWUWi6xteMP5nbTs7AxZQ90fApnHy9LqWRFpmH7ytaH3yVPrJ9WubOYk0IFIlBtZ-3BhHghat80dMan4EhhHwwTiTn-23hRxdBukY'];
+
+
+
+
+var message = new gcm.Message({
+    data: { key1: 'msg1' ,
+	    title: 'kekekekekekke' }
+});
+
+
 // configure app to use bodyParser()
 // this will let us get the data from a POST
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -24,8 +43,18 @@ router.post('/create/:desc', function(req, res){
 	res.json({message: 'u fucked up m8' } );
 });
 
-router.post('/ring/:id', function(req, res){
+router.get('/ring/:id', function(req, res){
 	res.json({message: 'DINGDONG' } );
+
+sender.send(message, { to: regTokens }, function (err, response) {
+    if(err) console.error(err);
+    else 	console.log(response);
+});
+
+
+
+
+
 });
 
 router.get('/list/', function(req, res) {
