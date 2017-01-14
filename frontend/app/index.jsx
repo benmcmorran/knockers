03b385057666cc991564e9d8e81955defb2120cb
Lambert Wang <lambert.ww@gmail.com>
2017-01-13 23:25:38 -0500

    Initialized react renderer

---
+++ frontend/app/index.jsx
@@ -0,0 +1,5 @@
+var React = require('react');
+var ReactDom = require('react-dom');
+var Root = require('../components/Root.jsx');
+
+ReactDom.render(<Root />, document.getElementById('app'));
---
+++ frontend/components/Root.jsx
@@ -0,0 +1,14 @@
+// components/Root.jsx
+var React = require('react')
+
+var Root = React.createClass({
+    render: function () {
+        return (
+            <p>
+                Hello, world!
+            </p>
+        )
+    }
+})
+
+module.exports = Root
