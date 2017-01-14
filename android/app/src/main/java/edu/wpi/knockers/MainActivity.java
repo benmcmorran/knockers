3e39b0a6eeae9015286211cfe3d9e90e0305588a
Ben McMorran <ben.mcmorran@gmail.com>
2017-01-13 23:46:43 -0500

    Firebase stuff

---
+++ android/app/src/main/java/edu/wpi/knockers/MainActivity.java
@@ -0,0 +1,31 @@
+package edu.wpi.knockers;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+
+public class MainActivity extends Activity {
+
+    private static final String TAG = "MainActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button = (Button)findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String token = FirebaseInstanceId.getInstance().getToken();
+                Log.d(TAG, "Token: " + token);
+                Toast.makeText(MainActivity.this, token, Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
